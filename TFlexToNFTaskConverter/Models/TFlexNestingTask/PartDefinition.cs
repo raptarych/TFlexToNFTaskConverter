6441fcc56fd35744dc7767427cdb52058d6feedc
Араптанов Артём <artem_a@holdingbp.ru>
2017-07-24 10:02:21 +0500

    Добавил реквизитов в модель детали

--- TFlexToNFTaskConverter/Models/TFlexNestingTask/PartDefinition.cs
+++ TFlexToNFTaskConverter/Models/TFlexNestingTask/PartDefinition.cs
@@ -14,13 +14,19 @@ namespace TFlexToNFTaskConverter.Models.TFlexNestingTask
     public class PartDefinition
     {
         public int ID { get; set; }
-        public int Count { get; set; }
+        public int Count { get; set; } = 1;
         public string FilePath { get; set; }
         [XmlElement]
         public PartProfile OriginalPartProfile { get; set; }
-        public double AngleStep { get; set; }
-        public bool DisableTurn { get; set; }
-        public bool OverturnAllowed { get; set; }
+
+        public double AngleStep { get; set; } = 0;
+        public bool DisableTurn { get; set; } = true;
+        public bool OverturnAllowed { get; set; } = false;
         public string Name { get; set; }
+        public string Notation { get; set; }
+        public int CountToStock { get; set; } = 0;
+        public bool Exclude { get; set; } = false;
+        public bool IsLinearPart { get; set; } = false;
+        public bool InheritAngle { get; set; } = true;
     }
 }

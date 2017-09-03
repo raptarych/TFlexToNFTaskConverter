1b420ca135182974213f8c0c07b87cc664c889a8
Artem Araptanov <raptarych@gmail.com>
2017-09-03 21:46:58 +0500

    И ещё

--- TFlexToNFTaskConverter/TaskLoader.cs
+++ TFlexToNFTaskConverter/TaskLoader.cs
@@ -1,35 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 using TFlexToNFTaskConverter.Models;
-using TFlexToNFTaskConverter.Models.TFlexNestingTask;
 
 namespace TFlexToNFTaskConverter
 {
     class TaskLoader
     {
+        private void LogToConsole(TFlexTask entity)
+        {
+            Console.WriteLine($"{entity.Name} loaded into buffer!");
+            Console.WriteLine($"Name: {entity.Name}\nProject type: {entity.ProjectType}");
+            Console.WriteLine($"Parts count: {entity.Parts.Count}");
+            Console.WriteLine($"Sheets: {entity.Sheets.Count}");
+            Console.WriteLine($"Results: {entity.Results.Count}");
+        }
         public TFlexTask LoadTFlexTask(string fileName)
         {
             Console.WriteLine($"\nLoading {fileName}...");
             var deserializer = new XmlSerializer(typeof(TFlexTask));
             TextReader textReader = new StreamReader(fileName);
             var entity = (TFlexTask)deserializer.Deserialize(textReader);
-            Console.WriteLine($"{fileName} loaded into buffer!");
-            Console.WriteLine($"Name: {entity.Name}\nProject type: {entity.ProjectType}");
-            Console.WriteLine($"Parts count: {entity.Parts.Count}");
-            Console.WriteLine($"Sheets: {entity.Sheets.Count}");
-            Console.WriteLine($"Results: {entity.Results.Count}");
+            LogToConsole(entity);
             return entity;
         }
         public TFlexTask LoadNfTask(string dirName)
         {
             var reader = new NFTaskReader();
             var task = reader.Read(dirName);
+            LogToConsole(task);
             return task;
         }
     }
